24b03238aa1b050565b5aa48ffb5d88199fe2937
明 <@192.168.1.1>
2018-11-09 15:38:57 +0800

    上传注释

--- 20151104723_zhouming_house/src/main/java/com/qst/controller/JumpController.java
+++ 20151104723_zhouming_house/src/main/java/com/qst/controller/JumpController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class JumpController {
+	//将安全目录jsp上传到框架
 	@RequestMapping("/index")
 	public String index(){
 	return "index";
