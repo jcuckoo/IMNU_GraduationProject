aaa528c63ae604554063bbca1b73e33be2cc8b52
Administrator <Administrator@DG>
2018-11-14 14:42:13 +0800

    11.14

--- 20151104730_zhengtianyu_Blog/src/exception/HeaderExceptionaaaaaaa.java
+++ 20151104730_zhengtianyu_Blog/src/exception/HeaderExceptionaaaaaaa.java
@@ -9,17 +9,17 @@ package exception;
  * @date 2018年7月13日
  * @time 下午1:52:33
  */
-public abstract class HeaderException extends RuntimeException{
+public abstract class HeaderExceptionaaaaaaa extends RuntimeException{
 
 	private static final long serialVersionUID = 1L;
 
 	public abstract EnumValue<?> getEnumValue();
 
-    protected HeaderException(String message){
+    protected HeaderExceptionaaaaaaa(String message){
         super(message);
     }
 
-    protected HeaderException(String message,Throwable cause){
+    protected HeaderExceptionaaaaaaa(String message,Throwable cause){
         super(message,cause);
     }
 
