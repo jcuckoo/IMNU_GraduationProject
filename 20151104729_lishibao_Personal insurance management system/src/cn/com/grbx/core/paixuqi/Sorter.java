72b0f70aacf025662b1abdbc276464a2329d7f6e
李世宝 <@DESKTOP-JF7NA6L>
2018-11-14 14:47:41 +0800

    第一周 毕设 分页器  排序器

---
+++ 20151104729_lishibao_Personal insurance management system/src/cn/com/grbx/core/paixuqi/Sorter.java
@@ -0,0 +1,45 @@
+package cn.com.lhd.core.sort;
+
+import cn.com.lhd.core.ISort;
+
+/**
+ * 排序器
+ */
+public class Sorter implements ISort {
+
+	private String field;
+
+	private boolean asc = true;
+
+	public Sorter() {
+		super();
+	}
+
+	public Sorter(String field) {
+		this();
+		this.field = field;
+	}
+
+	public Sorter(String field, boolean asc) {
+		this(field);
+		this.asc = asc;
+	}
+
+	@Override
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String field) {
+		this.field = field;
+	}
+
+	@Override
+	public boolean isAsc() {
+		return asc;
+	}
+
+	public void setAsc(boolean asc) {
+		this.asc = asc;
+	}
+}
