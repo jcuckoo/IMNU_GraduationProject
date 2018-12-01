3890c379e65d3ca8aae87b83af4689a3aee77b64
Administrator <Administrator@DG>
2018-12-01 22:37:09 +0800

    12.1

---
+++ 20151104730_zhengtianyu_Blog/src/cn/com/lhd/core/ISort.java
@@ -0,0 +1,16 @@
+package cn.com.lhd.core;
+
+/**
+ * 排序接口
+ *
+ * @author luohaidian
+ * @date 2018年7月13日
+ * @time 下午1:40:05
+ */
+public interface ISort {
+
+    String getField();
+
+    boolean isAsc();
+
+}
---
+++ 20151104730_zhengtianyu_Blog/src/cn/com/lhd/core/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * @desc 
+ *
+ * @author luohaidian
+ * @date 2018年10月21日
+ * @time 上午21:02:02
+ */
+package cn.com.lhd.core;
