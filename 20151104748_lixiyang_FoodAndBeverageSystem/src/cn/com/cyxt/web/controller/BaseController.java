ad3c7687521c93bc4e7dfd7d346a210c444db20e
Administrator <Administrator@ACVA632NHSSRUIP>
2018-12-19 15:58:30 +0800

    第六周，1代码部分

---
+++ 20151104748_lixiyang_FoodAndBeverageSystem/src/cn/com/cyxt/web/controller/BaseController.java
@@ -0,0 +1,60 @@
+package cn.com.cyxt.web.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import cn.com.lhd.core.JsonViewData;
+import cn.com.lhd.core.ResultCode;
+
+/**
+ * 
+ * @desc 基础数据控制器
+ *
+ * @author luohaidian
+ * @date 2018年11月5日
+ * @time 下午8:47:43
+ */
+public abstract class BaseController {
+
+    protected Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    protected HttpServletRequest httpRequest;
+
+    protected HttpServletResponse httpResponse;
+
+    protected HttpSession httpSession;
+
+    protected ModelMap modelMap;
+
+    @ModelAttribute
+    protected void initSpringMvcApiModel(
+            HttpServletResponse httpResponse,
+            HttpSession httpSession,
+            ModelMap modelMap
+    ) {
+        this.httpResponse = httpResponse;
+        this.httpSession = httpSession;
+        this.modelMap = modelMap;
+    }
+
+    protected JsonViewData setJsonViewData(ResultCode resultCode) {
+        return new JsonViewData(resultCode);
+    }
+
+    protected JsonViewData setJsonViewData(ResultCode resultCode, String message) {
+        return new JsonViewData(resultCode,message);
+    }
+
+    protected JsonViewData setJsonViewData(Object data) {
+        return new JsonViewData(data);
+    }
+
+}
---
+++ 20151104748_lixiyang_FoodAndBeverageSystem/src/cn/com/cyxt/web/controller/CommonController.java
@@ -0,0 +1,50 @@
+package cn.com.cyxt.web.controller;
+
+import org.apache.shiro.SecurityUtils;
+
+import cn.com.lhd.core.utils.SysProperties;
+
+/**
+ * 
+ * @desc
+ *
+ * @author luohaidian
+ * @date 2018年11月5日
+ * @time 下午8:48:11
+ */
+public class CommonController extends BaseController {
+
+    /**
+     * 每页容量默认为10
+     */
+    protected static final int pageSize = 10;
+    
+    /**
+     * 用户类型
+     */
+    protected static final String USERTYPE_M = "管理员";
+    protected static final String USERTYPE_C = "普通用户";
+
+    /**
+     * 组装分页信息
+     *
+     * @param pageNo 页号
+     * @param dataCount 结果集
+     */
+    public void createPageVo(int pageNo, int pageSize, int dataCount){
+        PageBeanVO pageBeanVo = new PageBeanVO();
+        pageBeanVo.setPageSize(pageSize);
+        pageBeanVo.setCurrentPage(pageNo);
+        pageBeanVo.calculate(dataCount);
+        httpRequest.setAttribute(SysProperties.PAGEBEAN_VO, pageBeanVo);
+    }
+    
+    /**
+     * 获取用户ID
+     * @return
+     */
+    public User getCurrentUser(){
+    	return (User) SecurityUtils.getSubject().getSession().getAttribute(SysProperties.CURRENTUSER);
+    }
+
+}
