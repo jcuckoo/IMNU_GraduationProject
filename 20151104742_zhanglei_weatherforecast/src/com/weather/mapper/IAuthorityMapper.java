aaeb2df5581a684668d27e1a461862507c5f3295
ZiYe <ZiYe@DESKTOP-4JJ5FOO>
2019-01-30 20:59:47 +0800

    本周第一次提交

---
+++ 20151104742_zhanglei_weatherforecast/src/com/weather/mapper/IAuthorityMapper.java
@@ -0,0 +1,49 @@
+package com.weather.mapper;
+
+import java.util.List;
+import java.util.Set;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+
+import cn.com.lhd.core.IPage;
+import cn.com.lhd.core.ISort;
+import com.weather.entity.Authority;
+
+
+@Mapper
+@Repository
+public interface IAuthorityMapper {
+	
+	/**
+	 * 查询权限列表
+	 * 
+	 * @param authority
+	 * @param fields
+	 * @param sortSet
+	 * @param page
+	 * @return List
+	 */
+	public List<Authority> loads(
+			@Param("authority") Authority authority, 
+			@Param("fields") Set<String> fields, 
+			@Param("sorts") Set<ISort> sortSet, 
+			@Param("page") IPage page);
+	
+	/**
+	 * 查询权限数
+	 * 
+	 * @param authority
+	 * @return int
+	 */
+	public int loadCount(@Param("authority") Authority authority);
+	
+	/**
+	 * 根据Id查询
+	 * @param id
+	 * @return
+	 */
+	public Authority loadById(@Param("id") Long id);
+
+}
