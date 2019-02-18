ed9e249ad48304a78d22a365709b6b1f97a49e60
ASUS <ASUS@windows10.microdone.cn>
2019-02-18 09:28:33 +0800

    提交2

---
+++ 20151104727_wangzeyu_Food/src/com/model/Spchu.java
@@ -0,0 +1,333 @@
+package com.model;
+
+import java.util.Date;
+
+public class Spchu {
+	
+	private Integer spchuId;
+	
+	private String spchuName;
+	
+	private String spchuMark;
+	
+	private String spchuMark1;
+	
+	private String spchuMark2;
+	
+	private String spchuMark3;
+	
+	private Date spchuDate;
+	
+	private Date spchuDate1;
+	
+	private Integer spchuZong;
+	
+	private Double spchuJine;
+	
+	private Double spchuZe;
+	
+	private Integer spchuType;
+	
+	private Integer spchuType1;
+	
+	private Integer shangpinId;
+	
+	private String shangpinName;
+	
+	private Integer sptypeId;
+	
+	private String sptypeName;
+	
+	private Integer yonghuId;
+	
+	private String yonghuName;
+	
+	private Integer yhroleId;
+	
+	private String yhroleName;
+	
+	private Integer yhbumenId;
+	
+	private Integer userId;
+	
+	private String yhbumenName;
+	
+	private String userName;
+	
+	private Integer bumenId;
+	
+	private String bumenName;
+	
+	private Integer roleId;
+	
+	private String roleName;
+	
+	private String spchuImg;
+	
+	private String spchuImgName;
+	
+	private String spchuErweima;
+	
+	private String spchuErweimaName;
+	
+	public Integer getSpchuId() {
+		return spchuId;
+	}
+	
+	public void setSpchuId(Integer spchuId) {
+		this.spchuId = spchuId;
+	}
+	
+	public String getSpchuName() {
+		return spchuName;
+	}
+	
+	public void setSpchuName(String spchuName) {
+		this.spchuName = spchuName == null ? null : spchuName.trim();
+	}
+	
+	public String getSpchuMark() {
+		return spchuMark;
+	}
+	
+	public void setSpchuMark(String spchuMark) {
+		this.spchuMark = spchuMark == null ? null : spchuMark.trim();
+	}
+	
+	public String getSpchuMark1() {
+		return spchuMark1;
+	}
+	
+	public void setSpchuMark1(String spchuMark1) {
+		this.spchuMark1 = spchuMark1 == null ? null : spchuMark1.trim();
+	}
+	
+	public String getSpchuMark2() {
+		return spchuMark2;
+	}
+	
+	public void setSpchuMark2(String spchuMark2) {
+		this.spchuMark2 = spchuMark2 == null ? null : spchuMark2.trim();
+	}
+	
+	public String getSpchuMark3() {
+		return spchuMark3;
+	}
+	
+	public void setSpchuMark3(String spchuMark3) {
+		this.spchuMark3 = spchuMark3 == null ? null : spchuMark3.trim();
+	}
+	
+	public Date getSpchuDate() {
+		return spchuDate;
+	}
+	
+	public void setSpchuDate(Date spchuDate) {
+		this.spchuDate = spchuDate;
+	}
+	
+	public Date getSpchuDate1() {
+		return spchuDate1;
+	}
+	
+	public void setSpchuDate1(Date spchuDate1) {
+		this.spchuDate1 = spchuDate1;
+	}
+	
+	public Integer getSpchuZong() {
+		return spchuZong;
+	}
+	
+	public void setSpchuZong(Integer spchuZong) {
+		this.spchuZong = spchuZong;
+	}
+	
+	public Double getSpchuJine() {
+		return spchuJine;
+	}
+
+	public void setSpchuJine(Double spchuJine) {
+		this.spchuJine = spchuJine;
+	}
+	
+	public Double getSpchuZe() {
+		return spchuZe;
+	}
+	
+	public void setSpchuZe(Double spchuZe) {
+		this.spchuZe = spchuZe;
+	}
+	
+	public Integer getSpchuType() {
+		return spchuType;
+	}
+	
+	public void setSpchuType(Integer spchuType) {
+		this.spchuType = spchuType;
+	}
+	
+	public Integer getSpchuType1() {
+		return spchuType1;
+	}
+	
+	public void setSpchuType1(Integer spchuType1) {
+		this.spchuType1 = spchuType1;
+	}
+	
+	public Integer getShangpinId() {
+		return shangpinId;
+	}
+	
+	public void setShangpinId(Integer shangpinId) {
+		this.shangpinId = shangpinId;
+	}
+	
+	public String getShangpinName() {
+		return shangpinName;
+	}
+	
+	public void setShangpinName(String shangpinName) {
+		this.shangpinName = shangpinName == null ? null : shangpinName.trim();
+	}
+	
+	public Integer getSptypeId() {
+		return sptypeId;
+	}
+	
+	public void setSptypeId(Integer sptypeId) {
+		this.sptypeId = sptypeId;
+	}
+	
+	public String getSptypeName() {
+		return sptypeName;
+	}
+	
+	public void setSptypeName(String sptypeName) {
+		this.sptypeName = sptypeName == null ? null : sptypeName.trim();
+	}
+	
+	public Integer getYonghuId() {
+		return yonghuId;
+	}
+	
+	public void setYonghuId(Integer yonghuId) {
+		this.yonghuId = yonghuId;
+	}
+	
+	public String getYonghuName() {
+		return yonghuName;
+	}
+	
+	public void setYonghuName(String yonghuName) {
+		this.yonghuName = yonghuName == null ? null : yonghuName.trim();
+	}
+	
+	public Integer getYhroleId() {
+		return yhroleId;
+	}
+	
+	public void setYhroleId(Integer yhroleId) {
+		this.yhroleId = yhroleId;
+	}
+	
+	public String getYhroleName() {
+		return yhroleName;
+	}
+	
+	public void setYhroleName(String yhroleName) {
+		this.yhroleName = yhroleName == null ? null : yhroleName.trim();
+	}
+	
+	public Integer getYhbumenId() {
+		return yhbumenId;
+	}
+	
+	public void setYhbumenId(Integer yhbumenId) {
+		this.yhbumenId = yhbumenId;
+	}
+	
+	public String getYhbumenName() {
+		return yhbumenName;
+	}
+	
+	public void setYhbumenName(String yhbumenName) {
+		this.yhbumenName = yhbumenName == null ? null : yhbumenName.trim();
+	}
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	
+	public void setUserName(String userName) {
+		this.userName = userName == null ? null : userName.trim();
+	}
+	
+	public Integer getBumenId() {
+		return bumenId;
+	}
+	
+	public void setBumenId(Integer bumenId) {
+		this.bumenId = bumenId;
+	}
+	
+	public String getBumenName() {
+		return bumenName;
+	}
+	
+	public void setBumenName(String bumenName) {
+		this.bumenName = bumenName == null ? null : bumenName.trim();
+	}
+	
+	public Integer getRoleId() {
+		return roleId;
+	}
+	
+	public void setRoleId(Integer roleId) {
+		this.roleId = roleId;
+	}
+	
+	public String getRoleName() {
+		return roleName;
+	}
+	
+	public void setRoleName(String roleName) {
+		this.roleName = roleName == null ? null : roleName.trim();
+	}
+	
+	public String getSpchuImg() {
+		return spchuImg;
+	}
+	
+	public void setSpchuImg(String spchuImg) {
+		this.spchuImg = spchuImg == null ? null : spchuImg.trim();
+	}
+	
+	public String getSpchuImgName() {
+		return spchuImgName;
+	}
+	
+	public void setSpchuImgName(String spchuImgName) {
+		this.spchuImgName = spchuImgName == null ? null : spchuImgName.trim();
+	}
+	
+	public String getSpchuErweima() {
+		return spchuErweima;
+	}
+	
+	public void setSpchuErweima(String spchuErweima) {
+		this.spchuErweima = spchuErweima == null ? null : spchuErweima.trim();
+	}
+	
+	public String getSpchuErweimaName() {
+		return spchuErweimaName;
+	}
+	
+	public void setSpchuErweimaName(String spchuErweimaName) {
+		this.spchuErweimaName = spchuErweimaName == null ? null
+				: spchuErweimaName.trim();
+	}
+}
